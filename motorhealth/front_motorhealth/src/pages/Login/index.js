e660d9b41f60be61205609e346ab7f1ae3328915
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2023-03-05 18:06:09 -0300

    tela de login implementada, sujeita a mudanças

--- motorhealth/front_motorhealth/src/pages/Login/index.js
+++ motorhealth/front_motorhealth/src/pages/Login/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { View, Text, TextInput,  } from "react-native";
-import { ButtonInput, Container, Title } from "./styles";
+import { Button, ButtonText, Container, Input, Title, TextLogin } from "./styles";
 
 export default function Login() {
     return (
         <Container>
             
             <Title>Bem Vindo!!</Title>
-            <Title>Usuário</Title>
-            <ButtonInput placeholder="Nome do usuário" ></ButtonInput>
-            <Title >Senha</Title>
-            <ButtonInput placeholder="Senha do usuário" ></ButtonInput>
-
+            <TextLogin>Usuário</TextLogin>
+            <Input  placeholder="Nome do usuário" ></Input >
+            <TextLogin >Senha</TextLogin>
+            <Input  placeholder="Senha do usuário" ></Input >
+            <Button>
+                <ButtonText>Login</ButtonText>
+            </Button>
         </Container>
     );
 }
--- motorhealth/front_motorhealth/src/pages/Login/styles.js
+++ motorhealth/front_motorhealth/src/pages/Login/styles.js
@@ -1,29 +1,51 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-
-    flex:1;
+    flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: ${props => props.theme.bg}
+    background-color: #C0C0C0;
+    borter-radius: 50px;
 `;
 
+export const Input = styled.TextInput`
+    height: 40px;
+    width: 80%;
+    border-color: black;
+    border-bottom-width: 1px;
+    margin-vertical: 10px;
+    padding: 10px;
+    font-size: 18px;
+`;
+
+ export const Button = styled.TouchableOpacity`
+    height: 40px;
+    width: 80%;
+    borderRadius:50px;
+    paddingVertical:8px;
+    background-color: #3277CC;
+    justify-content: center;
+    align-items: center;
+    top: 30px;
+ 
+`;
+
+export const ButtonText = styled.Text`
+    color: #F5FCFF;
+    font-size: 18px;
+    font-weight: bold;
+`;
 export const Title = styled.Text`
 
-    fontSize: 25px;
-    
-    
+    font-size: 25px;
+    color: #FFFFFF;
+    font-weight: bold ;
+    bottom: 100px;
+
 `;
-
-export const ButtonInput = styled.TextInput`
-
-    borderBottomWidth: 1;
-    height: 40px;
-    fontSize:16px
-`;
-
-export const ContainerLogin =  styled.View`
-
+export const TextLogin = styled.Text`
+    font-size: 18px;
+    color: #FFFFFF;
+    font-weight: bold ;
     
-
 `;
