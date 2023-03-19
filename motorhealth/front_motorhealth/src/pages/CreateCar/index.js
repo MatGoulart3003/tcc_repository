b8d963018c5f30eeb22f4cba0edda793ef919ad7
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2023-03-18 21:06:33 -0300

    adicionando selects, e implementando logica para os 3 selects funcionarem

--- motorhealth/front_motorhealth/src/pages/CreateCar/index.js
+++ motorhealth/front_motorhealth/src/pages/CreateCar/index.js
@@ -1,35 +1,73 @@
-import React,{Component} from "react";
+import React,{useState, useEffect} from "react";
 import { Text, View, FlatList } from "react-native";
 import api from '../../Services/Api'
 import Carro from "./Model";
+import RNPickerSelect from 'react-native-picker-select';
 
-export default class CreateCar extends Component{
+export default function CreateCar() {  
+    const [marcas, setMarcas] = useState([])
+    const [modelos, setModelos] = useState([])
+    const [anos, setAnos] = useState ([])
+    let marca;
+    let modelo;
+    let ano;
 
-    constructor(props){
-        super(props);
-        this.state = {
-            carros: []
-        }
+    function getFromApi (url){
+        api.get(url)
+            .then(response => {
+                const formattedData = response.data.map(item => ({
+                    label: item.nome,
+                    value: item.codigo
+                }))
+                if(url === '/marcas'){
+                    setMarcas(formattedData);
+                }else if (marca != null){
+                    setModelos(formattedData)
+                }
+            
+        })
+        .catch(error => {
+            console.error(error)
+        })
     }
 
-    async componentDidMount(){
-        const response = await api.get('/marcas');
-        this.setState({
-            carros: response.data
-        });
+    useEffect(() => {
+        if (marcas === null){
+            getFromApi('/marcas')
+        }else if (modelo === null){
+            getFromApi(`${marca}/modelos`)
+        }else if (ano === null){
+            getFromApi(`${modelo}/anos`)
+        }        
+    }, [])
 
-    }
-
-    render(){
         return(
         <View>
-            <FlatList
-            data={this.state.carros}
-            keyExtractor={item => item.codigo}
-            renderItem={({item}) => <Carro data={item} />}
+            <Text>Selecione a marca do carro:</Text>
+            <RNPickerSelect
+                items={marcas}
+                placeholder={{label: 'Selecione a Marca:', value: null}}
+                onValueChange={(value)=>{
+                    marca = value
+                }}
+            />
+            <Text>Selecione o Modelo</Text>
+            <RNPickerSelect
+                items={modelos}
+                placeholder={{label: 'Selecione o Modelo:', value: null}}
+                onValueChange={(value)=>{
+                    modelo = value
+                }}
+            />
+            <Text>Selecione o Ano</Text>
+            <RNPickerSelect
+                items={anos}
+                placeholder={{label: 'Selecione o Ano:', value: null}}
+                onValueChange={(value)=>{
+                    ano = value
+                }}
             />
         </View>
     );
-    }
-    
 }
+    
