13e6d25b6ca83e33912502e0bd5790955ccdd414
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2023-04-01 18:01:47 -0300

    select funcionando corretamente

--- motorhealth/front_motorhealth/src/pages/CreateCar/index.js
+++ motorhealth/front_motorhealth/src/pages/CreateCar/index.js
@@ -4,99 +4,98 @@ import api from '../../Services/Api'
 import RNPickerSelect from 'react-native-picker-select';
 
 export default function CreateCar() {
-    const [marcas, setMarcas] = useState([])
-    const [modelos, setModelos] = useState([])
-    const [anos, setAnos] = useState([])
-    const [marcaSelecionada, setMarcaSelecionada] = useState([]);
-    const [modeloSelecionado, setModeloSelecionado] = useState([]);
-    const [anoSelecionado, setAnoSelecionado] = useState([]);
-
-    async function getMarcas() {
-        await api.get('/marcas')
-            .then(response => {
-
-                const formattedData = response.data.map(item => ({
-                    label: item.nome,
-                    value: item.codigo
-                }));
-                setMarcas(formattedData);
-
-            })
-            .catch(error => {
-                console.error(error);
-            });
-
-    }
-
-/*     function getModelos(idMarca) {
-        useEffect(() => {
-            api.get(`/marcas/${idMarca}/modelos`)
-                .then(response => {
-                    const formattedData = response.data.modelos.map(item => ({
-                        label: item.nome,
-                        value: item.codigo
-                    }));
-
-                    setModelos(formattedData);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }, []);
-
-    }
-
-    function getAnos(idMarca, idAno) {
-        useEffect(() => {
-
-            api.get(`/marcas/${idMarca}/modelos/${idAno}/anos`)
-                .then(response => {
-
-                    const formattedData = response.data.map(item => ({
-                        label: item.nome,
-                        value: item.codigo
-                    }));
-                    setAnos(formattedData);
-
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }, []);
-    } */
-    getMarcas();
-    // getModelos(59)   
-    // getAnos(59,2394);  
-
+    const [selectedOption1, setSelectedOption1] = useState('');
+    const [selectedOption2, setSelectedOption2] = useState('');
+    const [selectedOption3, setSelectedOption3] = useState('');
+    const [options1, setOptions] = useState([])
+    const [options2, setOptions2] = useState([]);
+    const [options3, setOptions3] = useState([]);
+    
+    const fetchMarcas = async () => {
+      try {
+        const response = await api.get('/marcas');
+        const formattedData = response.data.map(item => ({
+          label: item.nome,
+          value: item.codigo
+        }));
+        setOptions(formattedData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    
+    const fetchModelos = async () => {
+      if (selectedOption1) {
+        try {
+          const response = await api.get(`/marcas/${selectedOption1}/modelos`);
+          const formattedData = response.data.modelos.map(item => ({
+            label: item.nome,
+            value: item.codigo
+          }));
+          setOptions2(formattedData);
+          setSelectedOption2('');
+          setOptions3([]);
+          setSelectedOption3('');
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    
+    const fetchAnos = async () => {
+      if (selectedOption2) {
+        try {
+          const response = await api.get(`/marcas/${selectedOption1}/modelos/${selectedOption2}/anos`);
+          const formattedData = response.data.map(item => ({
+            label: item.nome,
+            value: item.codigo
+          }));
+          setOptions3(formattedData);
+          setSelectedOption3('');
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    
+    useEffect(() => {
+      fetchMarcas();
+    }, []);
+    
+    useEffect(() => {
+      fetchModelos();
+    }, [selectedOption1]);
+    
+    useEffect(() => {
+      fetchAnos();
+    }, [selectedOption2]);
+    
     return (
-        <View>
-            <Text>Selecione a marca do carro:</Text>
-            <RNPickerSelect
-                items={marcas}
-                placeholder={{ label: 'Selecione a Marca:', value: null }}
-                onValueChange={(value) => {
-                    setMarcaSelecionada(value);
-                }}
-            />
-            {/* <Text>Selecione o Modelo</Text>
-            <RNPickerSelect
-                items={modelos}
-                placeholder={{ label: 'Selecione o Modelo:', value: null }}
-                onValueChange={(value) => {
-                    setModeloSelecionado(value)
-
-                }}
-            />
-            <Text>Selecione o Ano</Text>
-            <RNPickerSelect
-                items={anos}
-                placeholder={{ label: 'Selecione o Ano:', value: null }}
-                onValueChange={(value) => {
-                    setAnoSelecionado(value)
-
-                }}
-            /> */}
-        </View>
+      <View>
+        <Text>Selecione a marca do carro:</Text>
+        <RNPickerSelect
+          items={options1}
+          placeholder={{ label: 'Selecione a Marca:', value: null }}
+          onValueChange={(value) => {
+            setSelectedOption1(value);
+          }}
+        />
+        <Text>Selecione o Modelo</Text>
+        <RNPickerSelect
+          onValueChange={value => setSelectedOption2(value)}
+          items={options2}
+          disabled={!selectedOption1}
+        />
+        <Text>Selecione o Ano</Text>
+        <RNPickerSelect
+          onValueChange={value => setSelectedOption3(value)}
+          items={options3}
+          disabled={!selectedOption2}
+        />
+    
+        <Text>{`Selected options: ${selectedOption1}, ${selectedOption2}, ${selectedOption3}`}</Text>
+    
+      </View>
     );
-}
+}    
 
