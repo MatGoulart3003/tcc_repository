0a56672dd6db33883ad80241b8fada3943366272
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2023-04-01 20:56:53 -0300

    mostrando resultado

--- motorhealth/front_motorhealth/src/pages/CreateCar/index.js
+++ motorhealth/front_motorhealth/src/pages/CreateCar/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, FlatList } from "react-native";
-import { Button } from "./styles";
+import { ButtonText, Container } from "./styles";
 
 import api from '../../Services/Api';
 import RNPickerSelect from 'react-native-picker-select';
@@ -12,19 +12,7 @@ export default function CreateCar() {
     const [options1, setOptions] = useState([])
     const [options2, setOptions2] = useState([]);
     const [options3, setOptions3] = useState([]);
-    const [carSelected, setCarSelected]= useState(null)
-    const [car, setCar] =  useState(null)
-
-    const createCarObj = (marca, modelo, ano, comb, codigoFipe, valor)=>{
-        return {
-            marca: marca,
-            modelo: modelo,
-            ano: ano,
-            comb: comb,
-            codigoFipe: codigoFipe,
-            valorMercado: valor
-        } 
-    }
+    const [carDetails, setCarDetails] = useState(null);
 
     const fetchMarcas = async () => {
       try {
@@ -34,6 +22,7 @@ export default function CreateCar() {
           value: item.codigo
         }));
         setOptions(formattedData);
+        setCarDetails(null)
       } catch (error) {
         console.error(error);
       }
@@ -66,48 +55,39 @@ export default function CreateCar() {
             value: item.codigo
           }));
           setOptions3(formattedData);
-          setSelectedOption3('');
+          
         } catch (error) {
           console.log(error);
         }
       }
     };
     
-    const fetchVeic = async () => {
+    const fetchCarDetails = async (marca, modelo, ano) => {
+        try {
+          const response = await api.get(`/marcas/${marca}/modelos/${modelo}/anos/${ano}`);
+          const formattedData = {
+            marcaCarro: response.data.Marca,
+            modeloCarro: response.data.Modelo,
+            anoCarro: response.data.AnoModelo,
+            combustivel: response.data.Combustivel,
+            preco: response.data.Valor,
+          };
+          return formattedData;
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      }
+      
+    const handleSelectedOptions = async () => {
         if (selectedOption1 && selectedOption2 && selectedOption3) {
-          try {
-            const response = await api.get(`/marcas/${selectedOption1}/modelos/${selectedOption2}/anos/${selectedOption3}`);
-            const formattedData = response.data.map(item => ({
-              marca: item.Modelo,
-              modelo: item.CodigoFipe,
-              ano: item.AnoModelo,
-              comb: item.Combustivel,
-              codigoFipe: item.CodigoFipe,
-              valorMercado: item.Valor
-
-            }));
-            console.log(formattedData)
-            setCarSelected(formattedData)
-          } catch (error) {
-            console.log(error);
-          }
+          const carData = await fetchCarDetails(selectedOption1, selectedOption2, selectedOption3);
+          setCarDetails(carData);
+          console.log(selectedOption1, selectedOption2, selectedOption3)
+          console.log(carData)
         }
-      };
-
-      const handleCreateCar = () => {
-        if (carSelected && carSelected.length > 0) {
-          const car = createCarObj(
-            carSelected[0].marca,
-            carSelected[0].modelo,
-            carSelected[0].ano,
-            carSelected[0].comb,
-            carSelected[0].codigoFipe,
-            carSelected[0].valorMercado
-          );
-          setCar(car);
-        }
-      };
-
+    }
+    
     useEffect(() => {
       fetchMarcas();
     }, []);
@@ -119,14 +99,14 @@ export default function CreateCar() {
     useEffect(() => {
       fetchAnos();
     }, [selectedOption2]);
-    
+
     useEffect(() => {
-        fetchVeic();
+        handleSelectedOptions();
       }, [selectedOption1, selectedOption2, selectedOption3]);
     
     return (
-      <View>
-        <Text>Selecione a marca do carro:</Text>
+      <Container>
+        <ButtonText>Selecione a marca do carro:</ButtonText>
         <RNPickerSelect
           items={options1}
           placeholder={{ label: 'Selecione a Marca:', value: null }}
@@ -134,27 +114,33 @@ export default function CreateCar() {
             setSelectedOption1(value);
           }}
         />
-        <Text>Selecione o Modelo</Text>
+        <ButtonText>Selecione o Modelo</ButtonText>
         <RNPickerSelect
+          placeholder={{label: 'Selecione o Modelo:', value: null }}  
           onValueChange={value => setSelectedOption2(value)}
           items={options2}
           disabled={!selectedOption1}
         />
-        <Text>Selecione o Ano</Text>
+        <ButtonText>Selecione o Ano e Combustivel</ButtonText>
         <RNPickerSelect
+          placeholder={{label: 'Selecione o Ano e Combustivel:'}}  
           onValueChange={value => setSelectedOption3(value)}
           items={options3}
           disabled={!selectedOption2}
         />
-    
-        <Button title="Criar Carro" onPress={handleCreateCar} />
-            {car && (
-                <Text>
-                Marca: {car.marca}, Modelo: {car.modelo}, Ano: {car.ano}
-                </Text>
+        {carDetails ? (
+            <View>
+                <ButtonText>Marca: {carDetails.marcaCarro}</ButtonText>
+                <ButtonText>Modelo: {carDetails.modeloCarro}</ButtonText>
+                <ButtonText>Ano: {carDetails.anoCarro}</ButtonText>
+                <ButtonText>Combustivel: {carDetails.combustivel}</ButtonText>
+                <ButtonText>Preço: {carDetails.preco}</ButtonText>
+            </View>
+            ) : (
+                 <ButtonText>Carregando resultado...</ButtonText>
         )}
-    
-      </View>
+              
+      </Container>
     );
 }    
 
--- motorhealth/front_motorhealth/src/pages/CreateCar/styles.js
+++ motorhealth/front_motorhealth/src/pages/CreateCar/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
+    padding-top: 10%
     align-items: center;
     background-color: #C0C0C0;
     borter-radius: 50px;
 `;
+
 
 export const Input = styled.TextInput`
     height: 40px;
